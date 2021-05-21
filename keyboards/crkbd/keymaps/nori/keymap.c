60ce052d4ec51c7d547c13f6bd044662155e0e37
Noriaki Okubo <noriakiokubo@hotmail.com>
2021-05-21 19:43:14 +0900

    bs spc key repeat

--- keyboards/crkbd/keymaps/nori/keymap.c
+++ keyboards/crkbd/keymaps/nori/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC,                      KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_EQL, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC,  KC_GRV, KC_BSLS,                      _______, _______, KC_LBRC, KC_RBRC,  KC_GRV, KC_BSLS,
+      KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC,  KC_GRV, KC_BSLS,                      KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC,  KC_GRV, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, _______, _______, _______, KC_LCBR, KC_LBRC,                      KC_RBRC, KC_RCBR, _______, _______, _______, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
